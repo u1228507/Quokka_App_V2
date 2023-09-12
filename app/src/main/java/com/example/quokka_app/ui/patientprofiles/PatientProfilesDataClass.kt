1b7eeb8c6b0154ae1a44ed622d510c46b54c752d
u1228507 <142941502+u1228507@users.noreply.github.com>
2023-09-11 22:52:54 -0600

    -Created the patient profiles fragment that will list all the patients currently in the database

---
+++ app/src/main/java/com/example/quokka_app/ui/patientprofiles/PatientProfilesDataClass.kt
@@ -0,0 +1,7 @@
+package com.example.quokka_app.ui.patientprofiles
+
+data class PatientProfilesDataClass(
+    var first_Name : String? = null,
+    var last_Name : String? = null,
+    var date_Of_Birth: String? = null
+)
