8668a8fc30d569cb2a6696ae23ab6aea32e6e372
miles <u1157494@utah.edu>
2023-09-27 19:14:33 -0600

    1st changes for "Miles_Branch"

---
+++ app/src/main/java/com/example/quokka_app/ui/apt_view/fragment_record_visit.kt
@@ -0,0 +1,48 @@
+package com.example.quokka_app.ui.apt_view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quokka_app.R
+import com.example.quokka_app.databinding.FragmentRecordVisitBinding
+
+class fragment_record_visit : AppCompatActivity() {
+
+    private lateinit var binding: FragmentRecordVisitBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentRecordVisitBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val postVisit = post_visit()
+        val preVisit = pre_visit()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, postVisit)
+            commit()
+        }
+
+        binding.btnPostB.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, postVisit)
+                commit()
+            }
+        }
+
+        binding.btnPreB.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, preVisit)
+                commit()
+            }
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/quokka_app/ui/apt_view/post_visit.kt
@@ -0,0 +1,25 @@
+package com.example.quokka_app.ui.apt_view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quokka_app.R
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [post_visit.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+class post_visit : Fragment(R.layout.fragment_post_visit) {
+
+
+
+
+
+
+}
